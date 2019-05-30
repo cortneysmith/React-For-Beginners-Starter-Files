dbe62f0a2ca7a272d9a81e017ce4ea9e0935d577
Cortney Smith <Cortney.Smith@bentley.com>
2019-05-30 11:11:48 -0500

    okay actually mid vid 13 now

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 import React from "react";
 import { render } from "react-dom";
 import Router from "./components/Router";
 import "./css/style.css";
 
 render(<Router />, document.querySelector("#main"));
-=======
-import React from 'react';
-import { render } from 'react-dom';
-import Router from './components/Router';
-import './css/style.css';
-
-render(<Router />, document.querySelector('#main'));
->>>>>>> 424e61c76ccc824d75cb4130f10773dd696d85bd
